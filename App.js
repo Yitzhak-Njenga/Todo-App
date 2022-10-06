99973f6cf2d7ac397abb11863e37a7f206279a53
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-10-06 15:43:37 +0300

    added a list of task

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+
+  const [todos,setTodos] = useState([
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play ps 5',key:'3'}
+  ])
+// Can't find variable: useState?
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* header */}
+      <View style={styles.container}>
+         {/* form */}
+         <View style={styles.list}>
+           <FlatList
+           data={todos}
+           renderItem={({item}) =>(
+             <Text>{item.text}</Text>
+           )}
+           />
+         </View>
+
+      </View>
     </View>
   );
 }
